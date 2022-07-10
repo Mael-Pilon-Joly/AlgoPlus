0d9cbd5b0db2caebcb00cf65b7b84dff7dce217c
Maël Pilon-Joly <peops_finding911@hotmail.com>
2022-07-10 06:24:14 -0400

    push attempt

--- angular_frontend/src/app/board/userboard/userboard.component.ts
+++ angular_frontend/src/app/board/userboard/userboard.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
 import { RequestResponseUser } from '../../models/requestresponselogin.model';
 import { User } from '../../models/user.model';
 import { ApiService } from '../../services/apiservices.service';
+import { FileservicesService } from '../../services/fileservices.service';
 
 @Component({
   selector: 'app-userboard',
@@ -10,25 +12,45 @@ import { ApiService } from '../../services/apiservices.service';
 })
 export class UserboardComponent implements OnInit {
 
-  constructor(private services: ApiService) { }
-  userDTO ={
-    username: "",
-    password: "",
-    rememberme: false,
-  }
-
+  showCV:boolean = false;  
   user: User = {
     id: 0,
     username:"",
+    avatar: {},
+    cv:{},
     roles:[]
   }
 
   requestResponse: RequestResponseUser = {
     user: this.user
   }
+
+   avatarUrl: any;
+   cvUrl: any;
+
+  constructor(private services: ApiService, private fileservices: FileservicesService) { }
+
+  async getProfile(): Promise<void> {
+    await  this.services.getProfil().then( res=> {
+      console.log(res);
+      this.requestResponse = res;
+      var urlAvatar = this.fileservices.convertBlobToImage(res.user?.avatar.data);
+      var urlCV = this.fileservices.convertBlobToText(res.user?.cv?.data )
+      console.log("url avatar:"+ urlAvatar)
+      console.log("url cv:" + urlCV)
+      this.avatarUrl = urlAvatar
+      this.cvUrl = urlCV
+    })
+  }
+
   ngOnInit(): void {
-    this.services.getUser.subscribe(user => this.requestResponse = user);
-    console.log()
+    this.getProfile();
+    this.services.getUser.subscribe(user => { 
+      this.requestResponse = user
+    });
+
+    
+ 
   }
 
 }
